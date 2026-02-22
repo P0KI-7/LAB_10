023bdceb333b6ceb00072560743f4cb7779ec29c
P0KI-7 <kmp111207@gmail.com>
2026-02-23 01:31:14 +0300

    add and remote step

--- src/MyPipeline.hpp
+++ src/MyPipeline.hpp
@@ -8,23 +8,33 @@
 
 
  template <class T>
+struct Step
+{
+    std::string name;
+    std::function<T(T)> func;
+};
+
+template <class T>
 class MyPipeline
 {
-    std::vector<Step> steps;
+    std::vector<Step<T>> steps;
 public:
     // Basic methods
     MyPipeline() : steps() {};
-    size_t size() const { return steps.size() };
-    bool empty() const { return steps.empty() };
-    void clear() { steps.clear() };
+    size_t size() const { return steps.size(); }
+    bool empty() const { return steps.empty(); }
+    void clear() { steps.clear(); }
 
     // Adding step
-    //template<class F>
-    //void addStep(const std::string& name, F func);
+    template<class F>
+    void addStep(const std::string& name, F func);
+
     // Remove step
-    //void removeStep(size_t index);
+    void removeStep(size_t index);
+    
     // Run pipeline
     //T run(T value) const;
+    
     // Trace
     //std::vector<T> trace(T value const);
 
@@ -33,11 +43,26 @@ public:
 
 };
  
- template <class T>
-struct Step
+
+template<class T>
+template<class F>
+void MyPipeline<T>::addStep(const std::string& name, F func)
 {
-    std::string name;
-    std::function<T(T)> func;
-};
+    if (name.empty()) {
+        ;
+    }
+    steps.push_back({name, std::function<T(T)>(func)});
+}
+
+template<class T>
+void MyPipeline<T>::removeStep(size_t index)
+{
+    if (index >= steps.size()) {
+        ;
+    }
+    steps.erase(steps.begin() + index);
+}
+
+
 
 #endif // MyPipeline_hpp
